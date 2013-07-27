a3a133a1083735c45df303a0edcc4716d8d3b374
SixArm <sixarm@sixarm.com>
2013-07-26 19:29:39 -0700

    Make comments postive

--- lib/sixarm_ruby_minitest_extensions/assertions.rb
+++ lib/sixarm_ruby_minitest_extensions/assertions.rb
@@ -6,21 +6,21 @@ Please see README
 module Minitest::Assertions
 
   ##
-  # Fails unless +exp+ is true.
+  # Succeeds when +exp+ is true.
 
   def assert_true exp, msg = nil
     assert_same exp, true, msg
   end
 
   ##
-  # Fails unless +exp+ is false.
+  # Succeeds when +exp+ is false.
 
   def assert_false exp, msg = nil
     assert_same exp, false, msg
   end
 
   ##
-  # Fails unless +obj+ responds to each method in +meths+.
+  # Succeeds when +obj+ responds to each method in +meths+.
 
   def assert_respond_to_all obj, meths, msg = nil
     meths.each{|meth|
