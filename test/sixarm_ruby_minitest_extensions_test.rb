2a2918d1e82d9ba88806f3b681d83ca7e0130532
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:44 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_minitest_extensions_test.rb
+++ test/sixarm_ruby_minitest_extensions_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_minitest_extensions'
+require "sixarm_ruby_minitest_extensions"
 
 ['assertions','expectations'].map{|x|
   require "sixarm_ruby_minitest_extensions_test/#{x}_test"
